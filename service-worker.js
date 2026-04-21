7a7400ab5bc8b90b114a872a0404570dfde046c6
micnat07-a11y <micnat07@gmail.com>
2026-04-21 22:26:14 +0700

    coding

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "mealprep-v2";
+
+const urlsToCache = [
+  "/PROJECT-CHELL/",
+  "/PROJECT-CHELL/index.html",
+  "/PROJECT-CHELL/manifest.json",
+  "/PROJECT-CHELL/icons/logo-192x192.png",
+  "/PROJECT-CHELL/icons/logo-512x512.png",
+  "/PROJECT-CHELL/icons/logo-maskable-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
